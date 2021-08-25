bfbc060ab617b325143678902f0f5b9deec98256
shpalushi <87542468+shpalushi@users.noreply.github.com>
2021-08-25 14:19:02 +0200

    Update views.py

--- api_data/views.py
+++ api_data/views.py
@@ -17,49 +17,49 @@ from klaviyo_integration.settings import BASE_DIR
 
 def get_course_data(course_sku):
     if course_sku == "course-v1:OXF+FIN+03-2021":
-        return ["Register", "Oxford Fintech", "31 March 2021"]
+        return ["Register", "Oxford Fintech", "31/032021"]
     elif course_sku == "course-v1:OXF+FIN+06-2021":
-        return ["Register", "Oxford Fintech", "9 June 2021"]
+        return ["Register", "Oxford Fintech", "09/06/2021"]
     elif course_sku == "course-v1:OXF+FIN+10-2021":
-        return ["Register", "Oxford Fintech", "13 Oct 2021"]
+        return ["Register", "Oxford Fintech", "13/10/2021"]
     elif course_sku == "course-v1:OXF+FIN+01-2022":
-        return ["Register", "Oxford Fintech", "19 January 2022"]
+        return ["Register", "Oxford Fintech", "19/01/2022"]
     elif course_sku == "course-v1:OXF+CYB+03-2021":
-        return ["Register", "Oxford Cyber Security", "31 March 2021"]
+        return ["Register", "Oxford Cyber Security", "31/03/2021"]
     elif course_sku == "course-v1:OXF+CYB+06-2021":
-        return ["Register", "Oxford Cyber Security", "16 June 2021"]
+        return ["Register", "Oxford Cyber Security", "16/06/2021"]
     elif course_sku == "course-v1:OXF+CYB+10-2021":
-        return ["Register", "Oxford Cyber Security", "13 Oct 2021"]
+        return ["Register", "Oxford Cyber Security", "13/10/2021"]
     elif course_sku == "course-v1:OXF+CYB+01-2022":
-        return ["Register", "Oxford Cyber Security", "12 January 2022"]
+        return ["Register", "Oxford Cyber Security", "12/01/2022"]
     elif course_sku == "course-v1:OXF+AIF+03-2021":
-        return ["Register", "Oxford AI in Finance", "26 May 2021"]
+        return ["Register", "Oxford AI in Finance", "26/05/2021"]
     elif course_sku == "course-v1:OXF+AIF+10-2021":
-        return ["Register", "Oxford AI in Finance", "13 Oct 2021"]
+        return ["Register", "Oxford AI in Finance", "13/10/2021"]
     elif course_sku == "course-v1:OXF+AIF+01-2022":
-        return ["Register", "Oxford AI in Finance", "19 January 2022"]
+        return ["Register", "Oxford AI in Finance", "19/01/2022"]
     elif course_sku == "course-v1:MIT+HVN+06-2021":
-        return ["Register", "MIT Leading Health Tech Innovation", "16 June 2021"]
+        return ["Register", "MIT Leading Health Tech Innovation", "16/06/2021"]
     elif course_sku == "course-v1:MIT+HVN+10-2021":
-        return ["Register", "MIT Leading Health Tech Innovation", "29 September 2021"]
+        return ["Register", "MIT Leading Health Tech Innovation", "29/09/2021"]
     elif course_sku == "course-v1:MIT+AIL+07-2021":
-        return ["Register", "MIT AI Leadership", "7 July 2021"]
+        return ["Register", "MIT AI Leadership", "07/07/2021"]
     elif course_sku == "course-v1:MIT+AIL+10-2021":
-        return ["Register", "MIT AI Leadership", "6 October 2021"]
+        return ["Register", "MIT AI Leadership", "06/10/2021"]
     elif course_sku == "TEST+TST101+2021_T1":
-        return ["Register", "Test Product", "22 September 2021"]
+        return ["Register", "Test Product", "22/09/2021"]
     elif course_sku == "course-v1:OXF+BCH+09-2021":
-        return ["Register", "Oxford Blockchain Strategy", "22 September 2021"]
+        return ["Register", "Oxford Blockchain Strategy", "22/09/2021"]
     elif course_sku == "course-v1:OXF+PLT+10-2021":
-        return ["Register", "Oxford Platforms and Digital Disruption Programme", "6 October 2021"]
+        return ["Register", "Oxford Platforms and Digital Disruption Programme", "06/10/2021"]
     elif course_sku == "course-v1:OXF+PLT+01-2022":
-        return ["Register", "Oxford Platforms and Digital Disruption Programme", "19 January 2022"]
+        return ["Register", "Oxford Platforms and Digital Disruption Programme", "19/01/2022"]
     elif course_sku == "course-v1:CAM+STF+10-2021":
-        return ["Register", "Cambridge Startup Funding Pre-seed to Exit Programme", "13 October 2021"]
+        return ["Register", "Cambridge Startup Funding Pre-seed to Exit Programme", "13/10/2021"]
     elif course_sku == "course-v1:CAM+REG+10-2021":
-        return ["Register", "Cambridge RegTech", "20 October 2021"]
+        return ["Register", "Cambridge RegTech", "20/10/2021"]
     elif course_sku == "course-v1:MIT+AIS+10-2021":
-        return ["Register", "MIT AI + Data Strategy", "20 October 2021"]
+        return ["Register", "MIT AI + Data Strategy", "20/10/2021"]
     else:
         return ["Register", "Registered directly", "No date"]
 
